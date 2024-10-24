4076804e811e20800c50efdcf09cf25c3486e670
Junior-Knoth <161972203+Junior-Knoth@users.noreply.github.com>
2024-10-24 20:37:47 +0000

    improved header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#df3454"
+      }
+    },
   },
   plugins: [],
 }
